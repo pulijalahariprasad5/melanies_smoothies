71884830ef21dea376e262f42cf9ec5b497e052c
pulijalahariprasad5 <pulijalahariprasad5@gmail.com>
2024-11-06 20:43:41 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,6 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect(
     'choose upto 5 ingredients:'
     , my_dataframe
-    , max_selection=5
 )
 
 if ingredients_list:
