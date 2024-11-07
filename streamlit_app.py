a96509ba33ac74a12a5dcb75b84f8ba9ebb8c0b7
pulijalahariprasad5 <pulijalahariprasad5@gmail.com>
2024-11-06 20:52:48 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,17 +33,15 @@ if ingredients_list:
 
   my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)   
-    
-    
-
-    
+ 
 
     
   time_to_insert = st. button ('Submit Order')
   if time_to_insert:
       session.sql(my_insert_stmt).collect()
       st.success('Your Smoothie is ordered!', icon="✅")
+
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)  
